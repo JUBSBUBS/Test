5d04cb668da7287dc3e788af6f2c1ab6f64c5336
Maxim <mc.kudryatich@mail.ru>
2016-03-18 21:22:12 +0500

    12312323

--- gittest/src/gittest/Gittest.java
+++ gittest/src/gittest/Gittest.java
@@ -10,12 +10,12 @@ package gittest;
  * @author Max
  */
 public class Gittest {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ///////
     }
     
 }
