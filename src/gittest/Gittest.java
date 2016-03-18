9254fad31f8c9bc8d44aaafd046e7ddc7b5181df
JUBS_BUBS <JUBS_BUBS@JUBS_BUBS-PC>
2016-03-18 21:15:41 +0500

    fnfnfnfnf

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -18,7 +18,7 @@ public class Gittest {
         // ipaniy GIT///treeeeeeeeesh
         System.out.println("Are you a programmer?");
         //Pryuviet
-        //123
+        //123123123
     }
     
 }
