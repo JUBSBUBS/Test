b3b993b7f4fb9c8e4231f677e117e04a333fa19a
JUBS_BUBS <JUBS_BUBS@JUBS_BUBS-PC>
2016-03-18 20:44:11 +0500

    S. A. S. I.

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author JUBS_BUBS
+ */
+public class Gittest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // ipaniy GIT///
+        System.out.println("Are you a programmer?");
+    }
+    
+}
